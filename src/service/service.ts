b6b3a38757e8313c2733f6a044276cf346aacd4a
Niroj Shah <nirojshah1998@gmail.com>
2026-08-04 21:48:08 +0530

    Changed the directory Path.

--- src/service/service.ts
+++
@@ -1,9 +0,0 @@
-import type { DocumentationResponse, RepoInfo } from "./data.model.js";
-
-
-interface Service{
-    getRepoInfo(packageName: string): Promise<RepoInfo>;
-    getDocumentation(user: string, repo: string ): Promise<DocumentationResponse>;
-}
-
-export default Service;
